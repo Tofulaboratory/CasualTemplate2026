34583576f45dda64523b7992a834604dc1a948a2
Tofulaboratory <tofulab1373@gmail.com>
2026-01-20 22:25:39 +0900

    update

---
+++ Assets/Scripts/Utils/SoundManager.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class AudioManager : SingletonMonoBehaviour<AudioManager>
+{
+    private Dictionary<string, AudioSource> _bgmSourceDic = new Dictionary<string, AudioSource>();
+    private AudioSource _seSource = null;
+    private Dictionary<string, AudioClip> _seClipDic = new Dictionary<string, AudioClip>();
+
+    private const string PrefsKeyMasterVolume = "master_volume";
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Initialize();
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    private void Initialize()
+    {
+        if (_bgmSourceDic.Count > 0) return;
+
+        //TODO 読み込みをResourcesから変更する
+        object[] bgmData = Resources.LoadAll("Audio/Exclude/BGM");
+        object[] seData = Resources.LoadAll("Audio/Exclude/SE");
+
+        foreach (AudioClip bgm in bgmData)
+        {
+            _bgmSourceDic[bgm.name] = gameObject.AddComponent<AudioSource>();
+            _bgmSourceDic[bgm.name].clip = bgm;
+        }
+
+        foreach (AudioClip se in seData)
+        {
+            _seClipDic[se.name] = se;
+        }
+
+        _seSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// seをならす
+    /// </summary>
+    public void PlaySE(string name, float volume = 1)
+    {
+        if (!_seClipDic.ContainsKey(name)) return;
+
+        _seSource.PlayOneShot(_seClipDic[name], volume * GetMasterVolume());
+    }
+
+    /// <summary>
+    /// bgmをならす
+    /// </summary>
+    public void PlayBGM(string name, bool isLoop = true, float volume = 1)
+    {
+        if (!_bgmSourceDic.ContainsKey(name)) return;
+        if (_bgmSourceDic[name].isPlaying) return;
+
+        _bgmSourceDic[name].loop = isLoop;
+        _bgmSourceDic[name].volume = volume * GetMasterVolume();
+        _bgmSourceDic[name].Play();
+    }
+
+    /// <summary>
+    /// bgmを止める
+    /// </summary>
+    public void StopBGM(string name)
+    {
+        if (!_bgmSourceDic.ContainsKey(name)) return;
+        if (!_bgmSourceDic[name].isPlaying) return;
+
+        _bgmSourceDic[name].Stop();
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        PlayerPrefs.SetFloat(PrefsKeyMasterVolume, value);
+
+        foreach (var item in _bgmSourceDic.Values)
+        {
+            item.volume = GetMasterVolume();
+        }
+        _seSource.volume = GetMasterVolume();
+    }
+
+    private float GetMasterVolume()
+    {
+        float volume = 0.5f;
+        if (PlayerPrefs.HasKey(PrefsKeyMasterVolume)) volume = PlayerPrefs.GetFloat(PrefsKeyMasterVolume);
+        return volume;
+    }
+}
