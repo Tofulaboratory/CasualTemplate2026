1f2e89928b49d3ffec97c354e927b1a807e9c8c0
Tofulaboratory <tofulab1373@gmail.com>
2026-01-23 02:04:18 +0900

    update

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -2,4 +2,17 @@ using UnityEngine;
 
 public class MainSceneManager : MonoBehaviour
 {
+    private MainGameStateHandler _mainGameStateHandler;
+    void Start()
+    {
+        _mainGameStateHandler = new MainGameStateHandler();
+        _mainGameStateHandler.Initialize();
+        _mainGameStateHandler.ChangeState(MainGameState.Init);
+
+    }
+
+    void OnDestroy()
+    {
+        _mainGameStateHandler.Dispose();
+    }
 }
