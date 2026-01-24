d3ae3ff388f1ce9072771cd32c23fb2963b3279f
Tofulaboratory <tofulab1373@gmail.com>
2026-01-24 19:33:38 +0900

    update

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -1,4 +1,6 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using R3;
 
 public class MainSceneManager : MonoBehaviour
 {
@@ -7,8 +9,19 @@ public class MainSceneManager : MonoBehaviour
     {
         _mainGameStateHandler = new MainGameStateHandler();
         _mainGameStateHandler.Initialize();
+        _mainGameStateHandler.MainGameState.Subscribe(state =>
+        {
+            switch (state)
+            {
+                case MainGameState.Init:
+                    break;
+                case MainGameState.Begin:
+                    break;
+                case MainGameState.End:
+                    break;
+            }
+        }).AddTo(this);
         _mainGameStateHandler.ChangeState(MainGameState.Init);
-
     }
 
     void OnDestroy()
